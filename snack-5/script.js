fa9f5dc9a73b79d30112e49d5e617d32bbf0b93a
CarloMart88 <carlo.martino1988@gmail.com>
2025-07-17 18:48:41 +0200

    feat: 5 snack

--- snack-5/script.js
+++ snack-5/script.js
@@ -2,3 +2,17 @@ const nums = [2, 8, 4, 7, 12, 87];
 
 // Crea un array a partire dall'array nums, che abbia solo i numeri pari
 // Risultato: [2, 8, 4, 12]
+
+
+const evenNums = nums.filter(element => {
+  return element % 2 === 0
+})
+
+console.log(evenNums)
+
+console.log(`------compatto--------`)
+
+const evenNums1 = nums.filter(element => element % 2 === 0)
+
+console.log(evenNums1)
+
