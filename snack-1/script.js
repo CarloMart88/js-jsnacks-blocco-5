e7a65435803cce940a5b53c19472ea2ab170b19d
CarloMart88 <carlo.martino1988@gmail.com>
2025-07-17 18:11:00 +0200

    feat: 1 snack

--- snack-1/script.js
+++ snack-1/script.js
@@ -2,3 +2,10 @@ const names = ['Edoardo', 'Simone', 'Francesco'];
 
 // Stampa in console tutti i nomi
 // Risultato: 'Edoardo', 'Simone', 'Francesco'
+
+
+names.forEach(element => {
+  console.log(element)
+});
+
+names.forEach(element => console.log(element))
